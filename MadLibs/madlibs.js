0c9105929f26f05f040d09dfcc376f50a1bcce86
hoanewton <caohoa86hp@gmail.com>
2014-12-10 21:07:55 -0500

    refactor 1, add controller

---
+++ MadLibs/madlibs.js
@@ -0,0 +1,16 @@
+
+angular.module("madlibs", [])
+	.controller("MadLibsCtrl", function($rootScope) {
+		$rootScope.personGender
+		$rootScope.personName = "Jane";
+		$rootScope.pronoun = "she";
+		$rootScope.jobTitle = "clerk";
+		$rootScope.tediousTask = "countring money";
+		$rootScope.dirtyTask = "putting a spider into coworkers coffee";
+		$rootScope.celebrity = "Justin Bieber";
+		$rootScope.uselessSkill = "chatting";
+		$rootScope.adjective = "hedious";
+		$rootScope.obnoxiousCelebrity = "Paris Hilton";
+		$rootScope.hugeNumber = 321;
+	});
+
