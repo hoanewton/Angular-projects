41b3b431b2f1cf7ed3038b58fef504c913b0cb51
hoanewton <caohoa86hp@gmail.com>
2014-12-12 09:32:49 -0500

    set up multiplication table

--- Multiplication-table/app.js
+++ Multiplication-table/app.js
@@ -1,4 +1,18 @@
 angular.module('myApp', [])
-  .controller('MultiplicationCtrl', function($scope) {
-      $scope.numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
- 	});
+  .controller('MultiplicationCtrl', function($scope, $attrs) {
+      function polulateNumbers (x) {
+      	var numbers = [];
+      	for (var i=0; i<x; i++) {
+      		numbers[i] = i+1;
+      	};
+      	return numbers;
+      }
+
+      $scope.compute = function(a, b) {
+            return a * b;
+      };
+      $scope.$watch('numberLimit', function(limit) {
+         $scope.numbers = polulateNumbers(limit);
+      });
+      $scope.numberLimit = $attrs.initialNumberLimit || 10;
+ });
