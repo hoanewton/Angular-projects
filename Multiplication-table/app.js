8d426d9d99af5340af160ac3a790053645960a48
hoanewton <caohoa86hp@gmail.com>
2014-12-13 12:04:52 -0500

    finish multiplication table code along

--- Multiplication-table/app.js
+++ Multiplication-table/app.js
@@ -1,5 +1,11 @@
 angular.module('myApp', [])
-  .controller('MultiplicationCtrl', function($scope, $attrs) {
+   .controller('DisplayCtrl', function($scope) {
+        $scope.$on('displayData', function(event, data) {
+            $scope.content = data;
+        });
+    })
+
+   .controller('MultiplicationCtrl', function($scope, $attrs, $rootScope) {
       function polulateNumbers (x) {
       	var numbers = [];
       	for (var i=0; i<x; i++) {
@@ -15,4 +21,21 @@ angular.module('myApp', [])
          $scope.numbers = polulateNumbers(limit);
       });
       $scope.numberLimit = $attrs.initialNumberLimit || 10;
+
+      var activeFactorA, activeFactorB;
+      $scope.setActiveFactors = function(a, b) {
+         activeFactorA = a;
+         activeFactorB = b;
+      };
+      $scope.matchesFactor = function (a, b) {
+         return a === activeFactorA || b === activeFactorB;
+      };
+
+      $scope.clearActiveFactors = function() {
+         activeFactorA = activeFactorB = null;
+      };
+
+      $scope.setActiveNumber = function(number) {
+        $rootScope.$broadcast('displayData', number);
+      };
  });
