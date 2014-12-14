aab4443578239a7d2c45cb62b83c25f0ce552297
hoanewton <caohoa86hp@gmail.com>
2014-12-14 11:03:08 -0500

    set up for refactor 2, enhance UI

---
+++ MadLibs/madLib.js
@@ -0,0 +1,42 @@
+angular.module('madLib', ['ngMessages'])
+   .controller('MadLibCtrl', function($scope) {
+   	
+        // $scope.$on('displayData', function(event, data) {
+        //     $scope.content = data;
+        // });
+    })
+
+   // .controller('MultiplicationCtrl', function($scope, $attrs, $rootScope) {
+   //    function polulateNumbers (x) {
+   //    	var numbers = [];
+   //    	for (var i=0; i<x; i++) {
+   //    		numbers[i] = i+1;
+   //    	};
+   //    	return numbers;
+   //    }
+
+   //    $scope.compute = function(a, b) {
+   //          return a * b;
+   //    };
+   //    $scope.$watch('numberLimit', function(limit) {
+   //       $scope.numbers = polulateNumbers(limit);
+   //    });
+   //    $scope.numberLimit = $attrs.initialNumberLimit || 10;
+
+   //    var activeFactorA, activeFactorB;
+   //    $scope.setActiveFactors = function(a, b) {
+   //       activeFactorA = a;
+   //       activeFactorB = b;
+   //    };
+   //    $scope.matchesFactor = function (a, b) {
+   //       return a === activeFactorA || b === activeFactorB;
+   //    };
+
+   //    $scope.clearActiveFactors = function() {
+   //       activeFactorA = activeFactorB = null;
+   //    };
+
+   //    $scope.setActiveNumber = function(number) {
+   //      $rootScope.$broadcast('displayData', number);
+   //    };
+ // });
