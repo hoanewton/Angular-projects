6100a83ec7691255cb48a39e74bf1de763c01806
hoanewton <caohoa86hp@gmail.com>
2015-01-03 11:00:32 -0500

    change form

--- MadLibs/madLib.js
+++ MadLibs/madLib.js
@@ -1,38 +1,34 @@
 angular.module('madLib', ['ngMessages'])
-   .controller('MadLibCtrl', ['$scope', function($scope) {
-   		$scope.submit = function(){ 
+   .controller('MadLibCtrl',function($scope) {
+   		$scope.generate = function(form){ 
 	   		if( $scope.madLibForm.$valid ) {
-	   		 console.log('The form is valid'); 
-
+	   		  
 	   		 $('.words-container').hide();
 	   		 $('.paragraph-container').show();
 	   		} else {
    			 console.log(madLibForm.$error);
 	   		 console.log('The form is invalid');
 
-	   		 $('.words-container').show();
-	   		 $('.paragraph-container').hide(); 
-
 	   		}
-   		},
+         }
 
-   		$scope.switchViews = function() {
-   			console.log('Trying to switch Views here. Damn it!')
-   			$scope.data.personName = "";
-   			$scope.data.jobTitle = "";
-   			$scope.data.tediousTask = "";
-   			$scope.data.dirtyTask = "";
-   			$scope.data.uselessSkill = "";
-   			$scope.data.celebrity = "";
-   			$scope.data.adjective = "";
-   			$scope.data.obnoxiousCelebrity = "";
-   			$scope.data.hugeNumber = "";
+   		$scope.reset = function() {
+   			console.log('Trying to switch Views here!')
+
+   			// $scope.data.personName = "";
+   			// $scope.data.jobTitle = "";
+   			// $scope.data.tediousTask = "";
+   			// $scope.data.dirtyTask = "";
+   			// $scope.data.uselessSkill = "";
+   			// $scope.data.celebrity = "";
+   			// $scope.data.adjective = "";
+   			// $scope.data.obnoxiousCelebrity = "";
+   			// $scope.data.hugeNumber = "";
    			$scope.madLibForm.$submitted = false;
-   			
-   			$('.words-container').show();
-	   		$('.paragraph-container').hide(); 
+            $scope.madLibForm.$setPristine();
    		}
 
-    }]);
+    });
 
-
+// <button class="btn btn-primary" type="reset" ng-click="captchaForm.$setPristine()">Reset</button>
+//   <button type="submit" id="submit_button" class="btn btn-success btn-primary" ng-click="doSubmit(captchaForm)">
