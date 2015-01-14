f595b9a9b3b12f6651958b952847f91283fded6a
hoanewton <caohoa86hp@gmail.com>
2015-01-13 21:24:34 -0500

    fix bug on toggle view before click submit button

--- MadLibs/madLib.js
+++ MadLibs/madLib.js
@@ -1,13 +1,13 @@
 angular.module('madLib', ['ngMessages'])
    .controller('MadLibCtrl',['$scope',function($scope) {
-     $scope.formInvalid = false;
+     $scope.formInvalid = true;
      
    		$scope.generateMadLib = function(form){ 
         if (form.$invalid){
           $scope.formInvalid = true;
           $scope.formSubmitted = true;
           $scope.formValid = false;
-          console.log(madLibForm.$invalid);
+          console.log(form.$invalid);
           console.log('Hey, it is not valid');
         } else {
           console.log('form is valid yo');
