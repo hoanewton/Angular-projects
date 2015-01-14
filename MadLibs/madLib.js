d1df84cedf7e7543cdd0560bf4422736011d9c28
hoanewton <caohoa86hp@gmail.com>
2015-01-14 09:29:13 -0500

    get gender generator to work

--- MadLibs/madLib.js
+++ MadLibs/madLib.js
@@ -2,17 +2,27 @@ angular.module('madLib', ['ngMessages'])
    .controller('MadLibCtrl',['$scope',function($scope) {
      $scope.formSubmitted = false;
      $scope.formInvalid = true;
-     $scope.gender = true;
-     
+     // $scope.gender = true;
+
    		$scope.generateMadLib = function(form){ 
         if (form.$invalid){
           $scope.formInvalid = true;
           $scope.formSubmitted = true;
           $scope.formValid = false;
-          console.log(form.$invalid);
+
           console.log('Hey, it is not valid');
         } else {
           console.log('form is valid yo');
+          if($scope.data.gender == 'male'){
+            $scope.data.pronoun = "he";
+            $scope.data.pPronoun = "his";
+            $scope.data.pn = "him";
+          } else {
+            $scope.data.pronoun = "she";
+            $scope.data.pPronoun = "her";
+            $scope.data.pn = "her";
+          }
+          console.log()
           $scope.formInvalid = false;
           $scope.formValid = true; // always needs tobe set to "true" like this
         }
@@ -21,11 +31,11 @@ angular.module('madLib', ['ngMessages'])
    		$scope.reset = function() {
         console.log('reset bro')
         $scope.madLibForm.$setPristine();
-        // $scope.formValid = false;
-        // $scope.formSubmitted = false;
-        // $scope.formInvalid = false;
+        $scope.formValid = false;
+        $scope.formSubmitted = false;
+        $scope.formInvalid = true;
         $scope.data = {};
-        $scope.gender = true;
+        // $scope.gender = true;
    		};
       
     }]);
